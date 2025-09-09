760d8282d07e2685186a0c4dbdd83a3303291e0d
SashaWiscon <108764697+SashaWiscon@users.noreply.github.com>
2025-09-09 22:14:06 +0300

    select3

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { fetchBreeds, fetchCatByBreed } from './cat-api';
 import SlimSelect from 'slim-select';
-import 'slim-select/styles';
+import 'slim-select/dist/slimselect.css';
 
 const breedSelect = document.querySelector('.breed-select');
 const loader = document.querySelector('.loader');
