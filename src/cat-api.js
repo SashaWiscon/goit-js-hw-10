0753424ed82b3d817d8ff2605b3c651466430957
SashaWiscon <108764697+SashaWiscon@users.noreply.github.com>
2025-09-09 19:00:32 +0300

    error processing

--- src/cat-api.js
+++ src/cat-api.js
@@ -13,7 +13,7 @@ function fetchBreeds() {
   };
   
   return axios
-    .get(`${BASE_URL}/breeds`, options)
+    .get(`${BASE_URL}/breeds1`, options)
     .then(function (response) {
       console.log("all breeds", response.data);
       return response.data;
@@ -25,8 +25,6 @@ function fetchCatByBreed(breedId) {
   // const API_KEY =
   //   'live_6g45OnXhpbCWFGzduxcxKsKUseR6xEIkMC07o3PKvyfyhiz7tRYNxR75ufxEdJll';
   // const url = `https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`;
-
-  
   const options = {
     headers: {
       'x-api-key':
--- src/index.js
+++ src/index.js
@@ -1,43 +1,56 @@
 import { fetchBreeds, fetchCatByBreed } from './cat-api';
 
-
-const breedSelect = document.querySelector(".breed-select");
+const breedSelect = document.querySelector('.breed-select');
 const loader = document.querySelector('.loader');
-const error = document.querySelector('.error');
+const errorEl = document.querySelector('.error');
 const catContainer = document.querySelector('.cat-info');
 
 breedSelect.addEventListener('change', onSelect);
 
 fetchBreeds()
   .then(data => {
+    loader.style.display = 'block';
     const optionsMarkup = data
       .map(cat => `<option value ="${cat.id}">${cat.name}</option>`)
       .join('');
-    loader.style.display = 'none';
-    error.style.display = "none";
     breedSelect.innerHTML = optionsMarkup;
+    loader.style.display = 'none';
   })
   .catch(function (error) {
-    loader.style.display = 'block';
-    error.style.display = 'block';
+    loader.style.display = 'none';
+    errorEl.style.display = 'block';
     console.log(error);
     // throw new Error(`${error.response.status} ${error.response.statusText}`);
   });
 
-
 function onSelect(evt) {
-  evt.preventDefault();
-  
-  const selectedId = breedSelect.value;
-  console.log("eto select", selectedId);
-    
-  fetchCatByBreed(id)
-    .then(data => console.log('eto data', data))
+  catContainer.style.display = 'none';
+  errorEl.style.display = 'none';
+  loader.style.display = 'block';
+  const breedId = evt.target.value;
+  // console.log('eto evt target', breedId);
+
+  fetchCatByBreed(breedId)
+    .then(data => {
+      const markup = data
+        .map(
+          item => ` <img src = "${item.url}" alt="${item.breeds[0].name}">
+        <div>
+    <h2>${item.breeds[0].name}</h2>
+    <p>${item.breeds[0].description}</p>
+       <p>${item.breeds[0].temperament}</p>
+       </div>
+    `
+        )
+        .join('');
+
+      loader.style.display = 'none';
+      catContainer.style.display = 'flex';
+      catContainer.innerHTML = markup;
+    })
     .catch(function (error) {
-      console.log(error);
-    });  
-};
-
-
-
-
+      loader.style.display = 'none';
+      errorEl.style.display = 'block';
+      // console.log(error);
+    });
+}
